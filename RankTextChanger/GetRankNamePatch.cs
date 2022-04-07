56e70f53dcec3739bc8c98113b73aec1d64fc386
デンパ時計 <jsoldenpa@yahoo.co.jp>
2022-04-07 21:37:22 +0900

    プロジェクト ファイルを追加します。

---
+++ RankTextChanger/GetRankNamePatch.cs
@@ -0,0 +1,46 @@
+﻿using HarmonyLib;
+using System;
+
+namespace RankTextChanger
+{
+    [HarmonyPatch(
+        typeof(RankModel),
+        nameof(RankModel.GetRankName),
+        new Type[] { typeof(RankModel.Rank) }
+        )]
+    internal class GetRankNamePatch
+    {
+        public static void Postfix(RankModel.Rank rank, ref string __result)
+        {
+            switch (rank)
+            {
+                case RankModel.Rank.E:
+                    __result = "超悪";
+                    break;
+                case RankModel.Rank.D:
+                    __result = "悪";
+                    break;
+                case RankModel.Rank.C:
+                    __result = "微悪";
+                    break;
+                case RankModel.Rank.B:
+                    __result = "普通";
+                    break;
+                case RankModel.Rank.A:
+                    __result = "可";
+                    break;
+                case RankModel.Rank.S:
+                    __result = "良";
+                    break;
+                case RankModel.Rank.SS:
+                    __result = "超良";
+                    break;
+                case RankModel.Rank.SSS:
+                    __result = "神";
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
